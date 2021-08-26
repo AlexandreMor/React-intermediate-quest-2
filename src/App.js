7f84ea4247409be1e76389a808e07f23be61addf
Alexandre Morlat <alexandre-morlat_student2021@wilder.school>
2021-08-26 11:38:42 +0200

    Solved Quest React Intermediate 02 - Fetching data from an API within React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import DisplayQuote from './components/DisplayQuote';
+import axios from 'axios';
+import React from 'react';
 
 function App() {
+  const initialQuote = {
+    quote :	"Gah, stupid sexy Flanders!",
+    character	: "Homer Simpson",
+    image :	"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+    characterDirection :	"Right"
+    };
+    console.log(initialQuote)
+  const [quote, setQuote] = React.useState(initialQuote);
+  const getQuote = () => {
+
+    // Send the request
+  
+    axios
+  
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=5')
+  
+      // Extract the DATA from the received response
+  
+      .then((response) => response.data)
+  
+      // Use this data to update the state
+  
+      .then((data) => {
+  
+        setQuote(data[0]);
+  console.log(data[0]);
+      });
+  
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DisplayQuote quote={quote} />
+      <button type="button" onClick={getQuote}>Get Simpson quotes</button>
     </div>
   );
 }
---
+++ src/components/DisplayQuote.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function DisplayQuote({ quote }) {
+
+  return (
+
+    quote &&
+      <figure className="QuoteCard">
+        <img src={quote.image} alt={quote.character} />
+        <figcaption>
+          <blockquote>{quote.quote}</blockquote>
+          <p>
+            <cite>{quote.character}</cite>
+          </p>
+        </figcaption>
+      </figure>
+    );
+
+}
+
+
+export default DisplayQuote;
